abeb405d8cd7745995cf56f1fa66e98ca665a6b5
Luan Panno <luanpanno@gmail.com>
2021-05-22 01:35:06 -0300

    fix: reset css

--- frontend/src/assets/styles/GlobalStyles.ts
+++ frontend/src/assets/styles/GlobalStyles.ts
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-import { Reset } from './Reset';
-
 export const GlobalStyles = createGlobalStyle`
-  ${Reset}
 
   html, body {
     width: 100%;
